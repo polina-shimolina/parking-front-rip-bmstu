b2705f31edf10b41f9df336a6f352028be64b495
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2022-12-07 00:09:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const About = () => {
+    return (
+        <div>
+            <div><Link to='/'>Главная</Link> / <Link to="/about">О сервисе</Link>
+                <p></p></div>
+            <h2>О сервисе</h2>
+            <p></p>
+            <div><p>Инфо о сервисе</p>
+                <p>Еще инфо о сервисе</p></div>
+        </div>
+    )
 }
 
-export default App;
+
+export default About
